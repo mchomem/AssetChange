f342a8f32060a593330b96afcb7e054d237b6087
Misael Homem <misael.homem@gmail.com>
2023-03-21 18:16:09 -0300

    Implemented formatting of decimal values.

--- AssetChange.Infra.Data/Utils/BusinessUtil.cs
+++ AssetChange.Infra.Data/Utils/BusinessUtil.cs
@@ -3,6 +3,6 @@
     public static class BusinessUtil
     {
         public static string CalculateAndFormatPriceChange(decimal? newValue, decimal? oldValue)
-            => $" {((newValue - oldValue) / oldValue) * 100}%";
+            => $" {((newValue - oldValue) / oldValue * 100)?.ToString("0.00")}%";
     }
 }
